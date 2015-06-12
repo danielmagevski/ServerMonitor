a029cfbfe7fcd311ec2ee61d412b75d617fa33a5
daniel <daniel@not>
2015-06-12 00:58:02 -0300

    a

--- src/br/com/ServerMonitor/servermonitor.java
+++ src/br/com/ServerMonitor/servermonitor.java
@@ -1,13 +1,5 @@
 package br.com.ServerMonitor;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
